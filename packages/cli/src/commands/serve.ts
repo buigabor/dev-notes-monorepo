f1644f5b9b4d50fbcd512846245a913ace627e1d
Bui Gábor <bui.gabor97@gmail.com>
2021-03-10 18:45:36 +0100

    Add local-only API, add savings and loading cells, setup for NPM publishing

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,8 @@
+import { serve } from '@dev-notes-bui/local-api';
 import { Command } from 'commander';
-import { serve } from 'local-api';
 import path from 'path';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const serveCommand = new Command()
   .command('serve [filename] []')
@@ -9,7 +11,12 @@ export const serveCommand = new Command()
   .action(async (filename = 'notebook.js', options: { port: string }) => {
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
-      await serve(parseInt(options.port), path.basename(filename), dir);
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction,
+      );
       console.log(
         `Opened ${filename}. Navigate to http://localhost:${options.port} to edit file`,
       );
